249038ab1fbe57e81d77335c562f09caea32f8dc
thirdeyeclub <wwwaaavvveeerrr@gmail.com>
2019-04-15 16:10:12 -0400

    database created

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,20 @@
-// Update with your config settings.
-
 module.exports = {
 
   development: {
     client: 'sqlite3',
     connection: {
       filename: './dev.sqlite3'
-    }
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    },
+      useNullAsDefault: true, 
+      migrations: {
+        directory: './database/migrations',
+      },
+      seeds: {
+        directory: './database/seeds',
+      }
   },
 
   staging: {
