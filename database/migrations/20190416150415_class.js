9b03f1c44c59529f0245a9c8da34796c1aeddee5
thirdeyeclub <wwwaaavvveeerrr@gmail.com>
2019-04-16 15:28:46 -0400

    added classes database

---
+++ database/migrations/20190416150415_class.js
@@ -0,0 +1,25 @@
+exports.up = function(knex) {
+    return knex.schema.createTable('class', c => {
+    c.increments();
+    c.integer('class_id').notNullable().unique();
+
+    c.integer('price').notNullable();
+    c.string('name', 255).notNullable();
+    c.date('startDate');
+    c.string('schedule', 500); //lower this value later
+    c.string('location',500);//lower this value later
+    c.string('zipcode',10);//lower this value later
+    c.string('description',500);
+    c.integer('uses').notNullable();
+    c.timestamps(true, true);
+    c.integer('instructor_id').notNullable()
+    .references("role")
+    .inTable("users")
+    .onDelete("RESTRICT")
+    .onUpdate("CASCADE"); //foreign key linking instructors to classes they have created
+});
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTableIfExists('class');
+};
